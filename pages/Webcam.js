cea912d2f1d066a6f25bded4125a9018116551eb
voinar <nifeprty@gmail.com>
2024-02-17 20:12:50 +0100

    init project

---
+++ pages/Webcam.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import { useState } from 'react';
+import styles from '../styles/Webcam.module.css';
+import placeholder from '../img/placeholder.jpg';
+import Image from 'next/image';
+
+const WebcamComponent = () => <Webcam />;
+
+const videoConstraints = {
+  width: 1280,
+  height: 720,
+  facingMode: 'user',
+};
+
+export const WebcamCapture = () => {
+  const webcamRef = React.useRef(null);
+  const [image, setImage] = useState(placeholder);
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    console.log(imageSrc);
+    setImage(imageSrc);
+  }, [webcamRef]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.section}>
+        <Webcam
+          className={styles.image}
+          audio={false}
+          // height={220}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          // width={1280}
+          videoConstraints={videoConstraints}
+          onClick={capture}
+        />
+        <button onClick={capture}>Capture photo</button>
+      </div>
+      <div className={styles.section}>
+        <Image
+          className={styles.image}
+          src={image}
+          width={400}
+          height={400}
+          alt="scanner output"
+          onClick={() => setImage(placeholder)}
+        />
+        <span>Output</span>
+      </div>
+    </div>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,26 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import React from 'react';
+import Webcam from 'react-webcam';
+import { WebcamCapture } from './Webcam';
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>qr scanner</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          welcome to <a href="https://nextjs.org">qr scanner</a>
         </h1>
 
-        <p className={styles.description}>
+        <WebcamCapture />
+
+        {/* <p className={styles.description}>
           Get started by editing <code>pages/index.js</code>
         </p>
 
@@ -46,10 +52,9 @@ export default function Home() {
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
           </a>
-        </div>
+        </div> */}
       </main>
-
-      <footer>
+      {/* <footer>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -58,8 +63,7 @@ export default function Home() {
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
-      </footer>
-
+      </footer> */}
       <style jsx>{`
         main {
           padding: 5rem 0;
@@ -92,34 +96,17 @@ export default function Home() {
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
+          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
+            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
       `}</style>
-
       <style jsx global>{`
         html,
         body {
           padding: 0;
           margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
         * {
